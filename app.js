c44e6889a6eaca2832e4f8a829ec36e37f1c4cba
Brande George <george.brande@gmail.com>
2018-08-11 12:19:09 +0300

    passport

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ var promoRouter = require('./routes/promoRouter');
 var leadersRouter = require('./routes/leadersRouter');
 var session = require('express-session');
 var FileStore = require('session-file-store')(session);
+var passport = require('passport');
+var authenticate = require('./authenticate');
 
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
@@ -41,26 +43,23 @@ app.use(session({
 	store: new FileStore()
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 function auth (req, res, next) {
 	console.log(req.session);
 
-  if (!req.session.user) {
+  if (!req.user) {
     var err = new Error('You are not authenticated!');
-    err.status = 403;
+    res.setHeader('WWW-Authenticate', 'Basic'); 
+    err.status = 401;
     return next(err);
   }
   else {
-      if (req.session.user === 'authenticated') {
           return next();
-      }
-      else {
-          var err = new Error('You are not authenticated!');
-          err.status = 403;
-          return next(err);
-      }
   }
 }
 
